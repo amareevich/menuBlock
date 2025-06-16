ded36ae50d30fef903744a3cc6e829e30b5c86d3
Amareevich <amareevich@example.com>
2025-06-16 20:34:58 +0300

    view++

--- script.js
+++ script.js
@@ -4,8 +4,7 @@ document.addEventListener('click', (e) => {
         if (typeof clickMenu[el] === 'function') {
             clickMenu[el]();
         };
-
-    })
+    });
 });
 
 const clickMenu = {
@@ -14,10 +13,15 @@ const clickMenu = {
     'modal':document.querySelector('.modal'),
 
     toggle() {
-        this.menu.classList.toggle('menu--open');
-        this.menu.classList.toggle('menu--close');
-        // this.modal.classList.toggle('modal--open');
-        // this.modal.classList.toggle('modal--close');
+        if (this.menu.classList.contains('menu--open') || this.menu.classList.contains('menu--close')) {
+            this.menu.classList.toggle('menu--close');
+            this.menu.classList.toggle('menu--open');
+            this.modal.classList.toggle('modal--open');
+            this.modal.classList.toggle('modal--close')
+        } else {
+            this.menu.classList.toggle('menu--open');
+            this.modal.classList.toggle('modal--open');
+        };
     },
 
     'button--type--menu': function () {this.toggle()},
